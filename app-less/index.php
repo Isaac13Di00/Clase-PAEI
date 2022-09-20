19283a0f954cac36dcf1d2614bdfa8ac96f482db
Isaac Pérez <56978632+Isaac13Di00@users.noreply.github.com>
2022-09-20 10:06:09 -0600

    Add files via upload

--- app-less/index.php
+++ app-less/index.php
@@ -6,21 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" type="text/css" href="public/css/main.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
-    <div id="conteiner">
-        <form action="">
-            <fieldset>
-                <legend>
-                    Datos de acceso
-                </legend>
-                <label>Email: </label>
-                <input type="text" placeholder="Write here">
-                <label>Password: </label>
-                <input type="password" placeholder="* * * *">
-                <button type="submit">Acceder</button>
-        </fieldset>
-        </form>
+    <div id="conteiner" class="container ">
+        <div class="row pt-5">
+            <div class="col">
+                <div class="row ">
+                    <legend class="d-flex justify-content-center">
+                        Datos de acceso
+                    </legend>
+                </div>
+                <div class="row d-flex justify-content-center flex-column">
+                    <div class="col-sm-4">
+                        <label>Email: </label>
+                        <input type="text" placeholder="Write here">
+                    </div>
+                    <div class="col-sm-4">
+                        <label>Password: </label>
+                        <input type="password" placeholder="* * * *">
+                        <button type="submit">Acceder</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
 </html>
